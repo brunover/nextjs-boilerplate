9322dd1cc59820ea7e27e8db90a8dd9ab11e6a18
Bruno Leandro de Lima <brunoleandrodelima@gmail.com>
2022-02-07 14:56:20 -0800

    add a component generator

---
+++ .generate_component/component_templates.js
@@ -0,0 +1,35 @@
+// ---------------------
+// index.tsx
+exports.component = (name) => `import * as S from './styles'
+
+export type ${name}Props = {
+  props?: object
+}
+
+const ${name} = ({ props }: ${name}Props) => {
+  return <S.Wrapper>TODO: ${name}</S.Wrapper>
+}
+
+export default ${name}
+`
+
+// ---------------------
+// test.tsx
+exports.test = (name) => `import { render } from '@testing-library/react'
+
+import ${name} from '.'
+
+describe('<${name} />', () => {
+  test('it should render correctly', () => {
+    const { asFragment } = render(<${name} />)
+    expect(asFragment()).toBeInTheDocument()
+  })
+})
+`
+
+// ---------------------
+// style.ts
+exports.stylesheet = () => `import styled from 'styled-components'
+
+export const Wrapper = styled.div\`\`
+`
---
+++ .generate_component/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const { component, test, stylesheet } = require('./component_templates.js')
+
+// grab component name from terminal argument
+const [name] = process.argv.slice(2)
+if (!name) throw new Error('You must include a component name.')
+
+const dir = `./src/components/${name}/`
+
+console.log(`Creating component ${name} in: '${dir}'`)
+
+// throw an error if the file already exists
+if (fs.existsSync(dir))
+  throw new Error('A component with that name already exists.')
+
+// create the folder
+fs.mkdirSync(dir)
+
+function writeFileErrorHandler(err) {
+  if (err) throw err
+}
+
+// component.tsx
+fs.writeFile(`${dir}/index.tsx`, component(name), writeFileErrorHandler)
+// style.ts
+fs.writeFile(`${dir}/styles.ts`, stylesheet(), writeFileErrorHandler)
+// test.tsx
+fs.writeFile(`${dir}/test.tsx`, test(name), writeFileErrorHandler)
+
+/** Other files: */
+// component.scss
+// fs.writeFile(`${dir}/${name}.scss`, '', writeFileErrorHandler);
+// storybook.jsx
+// fs.writeFile(`${dir}/${name}.stories.jsx`, story(name), writeFileErrorHandler);
